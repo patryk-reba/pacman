2245404cfa34a4d23028532e7cb8e59f28e5a1a0
Patryk <patrykreba95@gmail.com>
2022-04-19 15:39:54 +0200

    update

--- index.js
+++ index.js
@@ -81,7 +81,6 @@ function control(e) {
     squares[pacmanCurrentIndex].classList.remove('pacman')
     switch (e.keyCode) {
         case 40:
-            console.log('pressed down')
             if (
                 !squares[pacmanCurrentIndex + width].classList.contains('ghost-lair') &&
                 !squares[pacmanCurrentIndex + width].classList.contains('wall') &&
@@ -90,7 +89,6 @@ function control(e) {
                 pacmanCurrentIndex += width
             break
         case 38:
-            console.log('pressed up')
             if (
                 !squares[pacmanCurrentIndex - width].classList.contains('ghost-lair') &&
                 !squares[pacmanCurrentIndex - width].classList.contains('wall') &&
@@ -99,7 +97,6 @@ function control(e) {
                 pacmanCurrentIndex -= width
             break
         case 37:
-            console.log('pressed left')
             if (
                 !squares[pacmanCurrentIndex - 1].classList.contains('ghost-lair') &&
                 !squares[pacmanCurrentIndex - 1].classList.contains('wall') &&
@@ -111,7 +108,6 @@ function control(e) {
             }
             break
         case 39:
-            console.log('pressed right')
             if (
                 !squares[pacmanCurrentIndex + 1].classList.contains('ghost-lair') &&
                 !squares[pacmanCurrentIndex + 1].classList.contains('wall') &&
@@ -187,10 +183,10 @@ ghosts.forEach(ghost => {
 ghosts.forEach(ghost => moveGhost(ghost))
 
 function moveGhost(ghost) {
-    console.log('moved ghost')
+
     const directions = [-1, +1, -width, +width]
     let direction = directions[Math.floor(Math.random() * directions.length)]
-    console.log(direction)
+
 
     ghost.timerId = setInterval(function () {
         //all our code
